adec86b104bf30249fd3ec2445f5bf89f7e57ec9
Thiago Valentim <thiagovalentim@hotmail.com.br>
2022-03-21 22:03:20 -0300

    Adicionado Collectionview
    Implementado a collectionview dentro da tableview
    Adicionado algumas ligacoes

--- Lava Jato/Features/Filtros/FiltroViewController.swift
+++ Lava Jato/Features/Filtros/FiltroViewController.swift
@@ -7,31 +7,82 @@
 
 import UIKit
 
-class FiltroViewController: UIViewController {
+class FiltroViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+   
 
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var callMapsButton: UIButton!
+    @IBOutlet weak var tViewTableView: UITableView!
+    
+    // Array para popular cell Element
+    var data:[ProfileGenre] = [
+        ProfileGenre(nameM: "Profissionais homem", nameF: "Profissionais mulher"),
+        ProfileGenre(nameM: "Profissionais homem", nameF: "Profissionais mulher")
+  ]
+    
+    // Array para popular cell Price
+    var listerPrice:[ProfilePrice] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        settingsTView()
+        configPrice()
     }
     
 
     @IBAction func callMaps(_ sender: Any) {
         performSegue(withIdentifier: "maps", sender: nil)
     }
+    // Método para popular cell
     
-    
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     
+   // Assinaturas
+    func settingsTView(){
+        self.tViewTableView.delegate = self
+        self.tViewTableView.dataSource = self
+        self.tViewTableView.register(ElementTableViewCell.nib(), forCellReuseIdentifier: ElementTableViewCell.identifier)
+        self.tViewTableView.register(PriceTableViewCell.nib(), forCellReuseIdentifier: PriceTableViewCell.identifier)
     }
-    */
+    
+    func configPrice(){
+        self.listerPrice.append(ProfilePrice(price: "Valor do serviço", priceMin: "R$20,00", priceMax: "R$800,00", place: "Onde você prefere fazer o serviço?"));
+        self.listerPrice.append(ProfilePrice(price: "Valor do serviço", priceMin: "R$20,00", priceMax: "R$800,00", place: "Onde você prefere fazer o serviço?"))
+    }
+    
+    // MARK: - métodos / extension
 
+    // Definindo o número de rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    // Populando as cell e definindo uma ordem
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ElementTableViewCell.identifier, for: indexPath) as? ElementTableViewCell
+            cell?.setupCell(profile: data[indexPath.row])
+            
+            return cell ?? UITableViewCell()
+        } else  {
+            let cellV = tableView.dequeueReusableCell(withIdentifier: PriceTableViewCell.identifier, for: indexPath) as? PriceTableViewCell
+            cellV?.setupCell(setup: self.listerPrice[indexPath.row])
+
+            return cellV ?? UITableViewCell()
+        }
+        
+    }
+    
+//     Definindo heigh
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if indexPath.row == 0{
+            return 166.0
+        } else {
+            return 466.0
+        }
+    }
+    
 }
+    
