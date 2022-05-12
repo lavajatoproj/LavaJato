13c34f34ddc0f4da3683f368402e445e656cdd64
lclaudiomattoss <lclaudiomattoss@gmail.com>
2022-05-12 15:50:00 -0300

    Criacao do MVVM
    Moldando a arquitetura do codigo para MVVM

--- Lava Jato/Features/MyServices/myServicesViewController.swift
+++ Lava Jato/Features/MyServices/myServicesViewController.swift
@@ -19,16 +19,26 @@ class myServicesViewController: UIViewController{
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var valueLabel: UILabel!
     
-    var listServices:[Services] =  []
+    private var viewModel:ServiceViewModel = ServiceViewModel()
     private var alert:AlertController?
+    
+    private func configTableView(){
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(CustomRowTableViewCell.nib(), forCellReuseIdentifier: CustomRowTableViewCell.identifier)
+    }
+    
+    func configInitials(){
+        self.upDateButton.layer.cornerRadius = 10
+        self.valueLabel.text = "-"
+        self.priceTF.isHidden = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.priceTF.isHidden = true
-        self.valueLabel.text = "-"
+        self.configInitials()
         self.addServices()
         self.configTableView()
-        self.upDateButton.layer.cornerRadius = 10
         self.alert = AlertController(controller: self)
     }
     
@@ -50,18 +60,9 @@ class myServicesViewController: UIViewController{
         })
     }
     
-    private func configTableView(){
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        self.tableView.register(CustomRowTableViewCell.nib(), forCellReuseIdentifier: CustomRowTableViewCell.identifier)
-    }
-    
     private func addServices(){
-        self.listServices.append(Services(opened: false, title: "Meus serviços", service: ["","Lavagem Simples", "Lavagem Completa", "Lavagem + Polimento","Lavagem a seco","Lavagem a vapor","Variação: Ecolavagem","Purificação de ar","Higienização"]))
-        self.listServices.append(Services(opened: false, title: "Formas de serviço", service: ["","Busco em casa", "Atendo no local"]))
-        self.listServices.append(Services(opened: false, title: "Preço", service: ["","Lavagem Simples", "Lavagem Completa", "Lavagem + Polimento","Lavagem a seco","Lavagem a vapor","Variação: Ecolavagem","Purificação de ar","Higienização"]))
+        self.viewModel.list()
     }
-    
 }
 
 
@@ -70,7 +71,7 @@ extension myServicesViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0{
             let cell = tableView.dequeueReusableCell(withIdentifier: CustomRowTableViewCell.identifier, for: indexPath) as? CustomRowTableViewCell
-            cell?.setupCell(product: self.listServices[indexPath.section].title)
+            cell?.setupCell(product: self.viewModel.listServices[indexPath.section].title)
             cell?.productLabel.textAlignment = .center
             cell?.back2View.layer.borderWidth = 0.0
             cell?.arrowImageView.isHidden = false
@@ -78,7 +79,7 @@ extension myServicesViewController: UITableViewDelegate, UITableViewDataSource{
             return cell ?? UITableViewCell()
         }else{
             let cell = tableView.dequeueReusableCell(withIdentifier: CustomRowTableViewCell.identifier, for: indexPath) as? CustomRowTableViewCell
-            cell?.setupCell(product: self.listServices[indexPath.section].service[indexPath.row])
+            cell?.setupCell(product: self.viewModel.listServices[indexPath.section].service[indexPath.row])
             cell?.switch.isHidden=false
             cell?.productLabel.textAlignment = .left
             cell?.back2View.layer.borderWidth = 2.0
@@ -95,12 +96,12 @@ extension myServicesViewController: UITableViewDelegate, UITableViewDataSource{
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return self.listServices.count
+        return self.viewModel.listServices.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if listServices[section].opened == true{
-            return self.listServices[section].service.count
+        if viewModel.listServices[section].opened == true{
+            return self.viewModel.listServices[section].service.count
         }else{
             return 1
         }
@@ -109,105 +110,24 @@ extension myServicesViewController: UITableViewDelegate, UITableViewDataSource{
     
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if listServices[indexPath.section].opened == true{
-            listServices[indexPath.section].opened = false
+        if viewModel.listServices[indexPath.section].opened == true{
+            viewModel.listServices[indexPath.section].opened = false
             let sections = IndexSet.init(integer: indexPath.section)
             tableView.reloadSections(sections, with: .none)
         }else{
-            listServices[indexPath.section].opened = true
+            viewModel.listServices[indexPath.section].opened = true
             let sections = IndexSet.init(integer: indexPath.section)
             tableView.reloadSections(sections, with: .none)
         }
         
-        
         if indexPath.section == 0{
-            if indexPath.row == 1{
-                self.serviceLabel.text = "Lavagem Simples"
-                self.valueLabel.text = "R$ 80,00"
-            }else{
-                if indexPath.row == 2{
-                    self.serviceLabel.text = "Lavagem Completa"
-                    self.valueLabel.text = "R$ 120,00"
-                }else{
-                    if indexPath.row == 3{
-                        self.serviceLabel.text = "Lavagem + Polimento"
-                        self.valueLabel.text = "R$ 250,00"
-                    }else{
-                        if indexPath.row == 4{
-                            self.serviceLabel.text = "Lavagem a seco"
-                            self.valueLabel.text = "R$ 130,00"
-                        }else{
-                            if indexPath.row == 5{
-                                self.serviceLabel.text = "Lavagem a vapor"
-                                self.valueLabel.text = "R$ 180,00"
-                            }else{
-                                if indexPath.row == 6{
-                                    self.serviceLabel.text = "Variação: Ecolavagem"
-                                    self.valueLabel.text = "R$ 170,00"
-                                }else{
-                                    if indexPath.row == 7{
-                                        self.serviceLabel.text = "Purificação de ar"
-                                        self.valueLabel.text = "R$ 140,00"
-                                    }else{
-                                        if indexPath.row == 8{
-                                            self.serviceLabel.text = "Higienização"
-                                            self.valueLabel.text = "R$ 90,00"
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-        if indexPath.section == 2{
-            if indexPath.row == 1{
-                self.serviceLabel.text = "Lavagem Simples"
-                self.priceTF.isHidden = false
-            }else{
-                if indexPath.row == 2{
-                    self.serviceLabel.text = "Lavagem Completa"
-                    self.priceTF.isHidden = false
-                }else{
-                    if indexPath.row == 3{
-                        self.serviceLabel.text = "Lavagem + Polimento"
-                        self.priceTF.isHidden = false
-                    }else{
-                        if indexPath.row == 4{
-                            self.serviceLabel.text = "Lavagem a seco"
-                            self.priceTF.isHidden = false
-                        }else{
-                            if indexPath.row == 5{
-                                self.serviceLabel.text = "Lavagem a vapor"
-                                self.priceTF.isHidden = false
-                            }else{
-                                if indexPath.row == 6{
-                                    self.serviceLabel.text = "Variação: Ecolavagem"
-                                    self.priceTF.isHidden = false
-                                }else{
-                                    if indexPath.row == 7{
-                                        self.serviceLabel.text = "Purificação de ar"
-                                        self.priceTF.isHidden = false
-                                    }else{
-                                        if indexPath.row == 8{
-                                            self.serviceLabel.text = "Higienização"
-                                            self.priceTF.isHidden = false
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            self.serviceLabel.text = self.viewModel.listServices[indexPath.section].service[indexPath.row]
+            self.valueLabel.text = self.viewModel.listServices[indexPath.section].price[indexPath.row]
+        } else if indexPath.section == 2{
+            self.serviceLabel.text = self.viewModel.listServices[indexPath.section].service[indexPath.row]
+            self.priceTF.isHidden = false
         }else{
             self.priceTF.isHidden = true
         }
     }
-    
-    
-    
-    
 }
--- Lava Jato/Features/RequestService/requestServiceViewController.swift
+++ Lava Jato/Features/RequestService/requestServiceViewController.swift
@@ -22,15 +22,19 @@ class requestServiceViewController: UIViewController {
     @IBOutlet weak var EndButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     
-    var listServices:[Services] =  []
+    var viewModel:RequestViewModel = RequestViewModel()
+    
     private var alert:AlertController?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    func configInitials(){
         self.EndButton.layer.cornerRadius = 10
         self.valueLabel.text = "-"
         self.serviceLabel.text = "-"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configInitials()
         self.configTableView()
         self.addService()
         self.alert = AlertController(controller: self)
@@ -60,16 +64,13 @@ class requestServiceViewController: UIViewController {
     }
     
     private func addService(){
-        self.listServices.append(Services(opened: false,title: "Selecione o serviço", service: ["","Lavagem Simples", "Lavagem Completa", "Lavagem + Polimento","Lavagem a seco","Lavagem a vapor","Variação: Ecolavagem","Purificação de ar","Higienização"]))
-        self.listServices.append(Services(opened: false, title: "Modo de retirada", service: ["","Levarei ao local", "Buscar aqui"]))
+        self.viewModel.listService()
     }
     
     func Style(){
         let textAtributes = [NSAttributedString.Key.foregroundColor:UIColor.ColorDefault]
         navigationController?.navigationBar.titleTextAttributes = textAtributes
     }
-    
-
 }
 
 extension requestServiceViewController: UITableViewDelegate, UITableViewDataSource{
@@ -79,24 +80,24 @@ extension requestServiceViewController: UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if listServices[section].opened == true{
-        return self.listServices[section].service.count
+        if viewModel.listServices[section].opened == true{
+            return self.viewModel.listServices[section].service.count
         }else{
             return 1
         }
-}
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0{
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellTableViewCell.identifier, for: indexPath) as? cellTableViewCell
-            cell?.setupCell(product: self.listServices[indexPath.section].title)
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellTableViewCell.identifier, for: indexPath) as? cellTableViewCell
+            cell?.setupCell(product: self.viewModel.listServices[indexPath.section].title)
             cell?.productLabel.textAlignment = .center
             cell?.back2View.layer.borderWidth = 0.0
             cell?.arrowImageView.isHidden = false
             return cell ?? UITableViewCell()
         }else{
             let cell = tableView.dequeueReusableCell(withIdentifier: cellTableViewCell.identifier, for: indexPath) as? cellTableViewCell
-            cell?.setupCell(product: self.listServices[indexPath.section].service[indexPath.row])
+            cell?.setupCell(product: self.viewModel.listServices[indexPath.section].service[indexPath.row])
             cell?.productLabel.textAlignment = .left
             cell?.back2View.layer.borderWidth = 2.0
             cell?.back2View.layer.borderColor = UIColor.ColorDefault.cgColor
@@ -113,71 +114,25 @@ extension requestServiceViewController: UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if listServices[indexPath.section].opened == true{
-            listServices[indexPath.section].opened = false
+        
+        if viewModel.listServices[indexPath.section].opened == true{
+            viewModel.listServices[indexPath.section].opened = false
             let sections = IndexSet.init(integer: indexPath.section)
             tableView.reloadSections(sections, with: .none)
         }else{
-            listServices[indexPath.section].opened = true
+            viewModel.listServices[indexPath.section].opened = true
             let sections = IndexSet.init(integer: indexPath.section)
             tableView.reloadSections(sections, with: .none)
         }
         
-        if indexPath.section == 1{
-            if indexPath.row == 1{
-                
-            }else{
-                
-            }
-        }
-        
         if indexPath.section == 0{
-            if indexPath.row == 1{
-                self.serviceLabel.text = "Lavagem Simples"
-                self.valueLabel.text = "R$ 80,00"
-            }else{
-                if indexPath.row == 2{
-                    self.serviceLabel.text = "Lavagem Completa"
-                    self.valueLabel.text = "R$ 120,00"
-                }else{
-                    if indexPath.row == 3{
-                        self.serviceLabel.text = "Lavagem + Polimento"
-                        self.valueLabel.text = "R$ 250,00"
-                    }else{
-                        if indexPath.row == 4{
-                            self.serviceLabel.text = "Lavagem a seco"
-                            self.valueLabel.text = "R$ 130,00"
-                        }else{
-                            if indexPath.row == 5{
-                                self.serviceLabel.text = "Lavagem a vapor"
-                                self.valueLabel.text = "R$ 180,00"
-                            }else{
-                                if indexPath.row == 6{
-                                    self.serviceLabel.text = "Variação: Ecolavagem"
-                                    self.valueLabel.text = "R$ 170,00"
-                                }else{
-                                    if indexPath.row == 7{
-                                        self.serviceLabel.text = "Purificação de ar"
-                                        self.valueLabel.text = "R$ 140,00"
-                                    }else{
-                                        if indexPath.row == 8{
-                                            self.serviceLabel.text = "Higienização"
-                                            self.valueLabel.text = "R$ 90,00"
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            self.serviceLabel.text = self.viewModel.listServices[indexPath.section].service[indexPath.row]
         }
         
-        
-        
-        
+        if indexPath.section == 0 && indexPath.row != 0{
+            self.valueLabel.text = self.viewModel.listServices[indexPath.section].price[indexPath.row]
+        }
     }
-    
-    
-    
 }
+
+
